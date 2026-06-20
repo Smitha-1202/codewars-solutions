b693471d54db1291a35e9af3b4feab5e72cf5343
Smitha <smitha-1202@users.noreply.github.com>
2026-06-21 02:41:49 +0530

    Add maths.py

---
+++ maths.py
@@ -0,0 +1,11 @@
+import math
+
+def round_it(n):
+    left, right = str(n).split('.')
+
+    if len(left) < len(right):
+        return math.ceil(n)
+    elif len(left) > len(right):
+        return math.floor(n)
+    else:
+        return round(n)
